8dcc33a5c086d1cf5abe411060fcd38d81659acc
Kamoji <bartvvenrooij@gmail.com>
2016-09-21 09:29:46 +0200

    dit is een test

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,7 +17,8 @@ class ArticleController extends Controller
 
     public function create()
     {
-        return view('article.create');
+        dd('test');
+//        return view('article.create');
     }
 
     public function store(Request $request)
