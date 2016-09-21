3b330f076da5944c134154ee861971670b478058
Kamoji <bartvvenrooij@gmail.com>
2016-09-21 14:23:05 +0200

    Added resources to the routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,7 @@
 
 Route::get('/', "IndexController@index");
 
-Route::get('/article', "ArticleController@index")->name('article.index');
-Route::get('/article/create', "ArticleController@create")->name('article.create');
-Route::get('/article/{article}', "ArticleController@show")->name('article.show');
-Route::get('/article/{article}/edit', "ArticleController@edit")->name('article.edit');
-Route::post('/article', "ArticleController@store")->name('article.store');
-Route::patch('/article/{article}', "ArticleController@update")->name('article.update');
-Route::delete('/article/{article}', "ArticleController@destroy")->name('article.destroy');
+Route::resource('article', "ArticleController");
 
 Route::get('/home', function () {
     return view('welcome');
